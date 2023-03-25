decfaeda16a93c3018f54ada82b1ce8eea08542c
Uneld <aleksey.minchenya@gmail.com>
2023-03-25 16:26:43 +0300

    added all tasks

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] SingleLineInput()
+{
+    System.Console.WriteLine("Enter single line array with a \"space\"");
+    return Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+
+int ReleaseArray(int[] arr)
+{
+    int count = 0;
+    foreach (int item in arr)
+    {
+        if (item > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+System.Console.Clear();
+int[] array = SingleLineInput();
+System.Console.WriteLine($"Positive count = {ReleaseArray(array)}");
---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] SingleLineInput()
+{
+    System.Console.WriteLine("Enter single line array with a \"space\"");
+    return Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+
+
+string ReleaseArray(int[] arr)
+{
+    if (arr.Length > 4)
+    {
+        return "Wrong enter";
+    }
+
+    double b1 = arr[0], k1 = arr[1], b2 = arr[2], k2 = arr[3];
+    double diffK = k1 - k2, diffB = b1 - b2;
+    if (diffK == 0)
+    {
+        return "Lines are not cross";
+    }
+
+    double x = Math.Round(-diffB / diffK, 2);
+    double y = Math.Round(k2 * x + b2, 2);
+    return $"Intersection point ({x}; {y})";
+}
+
+
+//-1 2 1 -3
+//0 1 -2 3
+//3 2 5 2
+//2 5 4 9
+System.Console.Clear();
+System.Console.WriteLine("Enter b1, k1, b2, k2");
+int[] array = SingleLineInput();
+System.Console.WriteLine(ReleaseArray(array));
